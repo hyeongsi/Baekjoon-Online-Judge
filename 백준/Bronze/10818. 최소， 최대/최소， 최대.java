30b130fd06ffb7ae8d03afdca76c6c4e11a51d2b
박시형 <71202869+hyeongsi@users.noreply.github.com>
2023-06-22 16:53:54 +0900

    [Bronze III] Title: 최소, 최대, Time: 504 ms, Memory: 87716 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,15 +1,13 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = null;
+		StringBuilder sb;
 		
 		int loop = Integer.parseInt(br.readLine());
 		st = new StringTokenizer(br.readLine());
@@ -19,18 +17,19 @@ public class Main {
 		int cmp;
 		for(int i=0; i<loop; i++) {
 			cmp = Integer.parseInt(st.nextToken());
-			if(max < cmp) {
+			
+			if(max < cmp) 
 				max = cmp;
-			}
-			if(min > cmp) {
+			
+			if(min > cmp) 
 				min = cmp;
-			}
 		}
-		
-		bw.write(min + " " + max);
-		bw.flush();
-		
-		bw.close();
 		br.close();
+		
+		sb = new StringBuilder();
+		sb.append(min).append(" ").append(max);
+		
+		System.out.println(sb.toString());
+		
 	}
 }
