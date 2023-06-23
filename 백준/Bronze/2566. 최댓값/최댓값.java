cd9f45791704f71f718670d145b3565fac2ec863
박시형 <71202869+hyeongsi@users.noreply.github.com>
2023-06-23 18:43:21 +0900

    [Bronze III] Title: 최댓값, Time: 148 ms, Memory: 15956 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -6,18 +6,17 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		String[][] matrix = new String[9][9];
 		int[] max = new int[3]; // 값, 행, 열
 		max[0] = -1;
 		
-		for(int i = 0; i < 9; i++) {
-				matrix[i] = br.readLine().split(" ");
-		}
-		
 		for(int i = 0; i < 9; i ++) {
+			String[] strArr = br.readLine().split(" ");
+			
 			for(int j = 0; j < 9; j ++) {
-				if(max[0] < Integer.parseInt(matrix[i][j])) {
-					max[0] = Integer.parseInt(matrix[i][j]);
+				int value = Integer.parseInt(strArr[j]);
+				
+				if(max[0] < value) {
+					max[0] = value;
 					max[1] = i + 1;
 					max[2] = j + 1;
 				}	
