1731cd58a39af9b21ac8541255d1482960b932d3
박시형 <71202869+hyeongsi@users.noreply.github.com>
2023-06-25 14:28:23 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 128 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		long n = Long.parseLong(br.readLine());
+		n-=2;
+		
+		long sum = 0;
+		
+		for(long l = 1; l <= n; l++) {
+			sum += l * (l+1) / 2;
+		}
+		
+		System.out.println(sum);
+		System.out.println(3);
+	}
+}
