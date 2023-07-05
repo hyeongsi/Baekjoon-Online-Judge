83d10f8a99db6f8b9de560c966f2ea7aefa60c1a
박시형 <71202869+hyeongsi@users.noreply.github.com>
2023-07-05 23:15:28 +0900

    [Silver II] Title: 좌표 압축, Time: 2096 ms, Memory: 292736 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,44 +10,30 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		
-		Pos[] p = new Pos[n];
+		int[] arr1 = new int[n];
+		int[] arr2 = new int[n];
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < n; i++) {
-			p[i] = new Pos();
-			p[i].value = Integer.parseInt(st.nextToken());
-			p[i].index = i;
+			arr1[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		Arrays.sort(p, (o1, o2)->{
-			return o1.value - o2.value;
-		});
+		System.arraycopy(arr1, 0, arr2, 0, arr1.length);
 		
-		int max = Integer.MIN_VALUE;
-		int cp = -1;
-		for(int i = 0; i < n; i++) {
-			if(max < p[i].value) {
-				max = p[i].value;
-				cp++;
+		Arrays.sort(arr2);
+		HashMap<Integer, Integer> hashMap = new HashMap<>();
+		
+		int r = 0;
+		for(int v : arr2) {
+			if(!hashMap.containsKey(v)) {
+				hashMap.put(v, r++);
 			}
-			
-			p[i].compress = cp;
 		}
-		
-		Arrays.sort(p, (o1, o2)->{
-			return o1.index - o2.index;
-		});
 		
 		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < n; i++) {
-			sb.append(p[i].compress + " ");
+		for(int k : arr1) {
+			sb.append(hashMap.get(k) + " ");
 		}
 		
 		System.out.println(sb);
 	}
-}
-
-class Pos{
-	int value;
-	int index;
-	int compress;
 }
