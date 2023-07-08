a54a886e9068496fd2d724b362435a0e60e3cb19
박시형 <71202869+hyeongsi@users.noreply.github.com>
2023-07-08 10:54:33 +0900

    [Silver IV] Title: 가로수, Time: 260 ms, Memory: 22752 KB -BaekjoonHub

--- 백준/Silver/2485. 가로수/가로수.java
+++ 백준/Silver/2485. 가로수/가로수.java
@@ -1,41 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int loop = Integer.parseInt(br.readLine());
-		
-		int[] tree = new int[loop];
-		ArrayList<Integer> distanceArr = new ArrayList<>();
-		
-		for(int i = 0; i < loop; i++) {
-			tree[i] = Integer.parseInt(br.readLine());
-			
-			if(i != 0) {
-				distanceArr.add(tree[i] - tree[i-1]);
-			}	
+
+		int[] arr = new int[loop];
+		for (int i = 0; i < loop; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
 		}
-		
-		
-		int g = distanceArr.get(0);
-		for(int i = 0; i < distanceArr.size()-1; i++) {
-			g = gcd(g, distanceArr.get(i+1));
+
+		Arrays.sort(arr);
+
+		int minGcd = 0;
+		for (int i = 0; i < arr.length - 1; i++) {
+			int value = arr[i + 1] - arr[i];
+			minGcd = gcd(value, minGcd);
 		}
-		
-		int end = tree[loop-1] - tree[0];
-		System.out.println(end / g + 1 - loop);
+
+		System.out.println(((arr[arr.length - 1] - arr[0]) / minGcd + 1) - arr.length);
 	}
 
 	public static int gcd(int a, int b) {
-		if (a < b) {
-			int tmp = a;
-			a = b;
-			b = tmp;
-		}
-
 		if (b == 0)
 			return a;
 		else
