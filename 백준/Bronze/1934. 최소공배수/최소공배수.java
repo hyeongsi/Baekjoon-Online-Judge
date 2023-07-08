ff33ab76ffad3e941b0dcb52d6716e02404337f8
박시형 <71202869+hyeongsi@users.noreply.github.com>
2023-07-08 10:13:09 +0900

    [Bronze I] Title: 최소공배수, Time: 172 ms, Memory: 16224 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.java
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -7,29 +7,29 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int loop = Integer.parseInt(br.readLine());
-		
+
 		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < loop; i++) {
+		for (int i = 0; i < loop; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			
-			sb.append(lcm(a,b) + "\n");
+
+			sb.append(a * b / gcd(a, b) + "\n");
 		}
-		
+
 		System.out.println(sb);
 	}
-	
-	public static int lcm(int a, int b) {
-		int min = Math.min(a, b);
-		int max = Math.max(a, b);
-		
-		int i = 1;
-		int result = 0;
-		while(true) {
-			result = min * i++;
-			if(result % max == 0)
-				return result;
+
+	public static int gcd(int a, int b) {
+		if (a < b) {
+			int tmp = a;
+			a = b;
+			b = tmp;
 		}
+
+		if (b == 0)
+			return a;
+		else
+			return gcd(b, a % b);
 	}
 }
