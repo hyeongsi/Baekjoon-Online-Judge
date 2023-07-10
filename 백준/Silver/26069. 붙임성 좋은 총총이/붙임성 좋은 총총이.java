962954e50eba6bfc4046ed11ac1d56dd8c54ec4d
박시형 <71202869+hyeongsi@users.noreply.github.com>
2023-07-10 16:15:26 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 148 ms, Memory: 14648 KB -BaekjoonHub

---
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int loop = Integer.parseInt(br.readLine());
+
+		HashSet<String> set = new HashSet<>();
+		set.add("ChongChong");
+		for(int l = 0; l < loop; l++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String p1 = st.nextToken();
+			String p2 = st.nextToken();
+			
+			if(set.contains(p1))
+				set.add(p2);
+			if(set.contains(p2))
+				set.add(p1);
+		}
+		
+		System.out.println(set.size());
+	}
+}
