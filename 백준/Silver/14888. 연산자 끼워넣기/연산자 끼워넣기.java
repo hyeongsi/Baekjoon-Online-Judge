dc73d938d906554db4efa9eed76449b1de416029
박시형 <71202869+hyeongsi@users.noreply.github.com>
2023-07-15 12:04:32 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 132 ms, Memory: 15480 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -3,84 +3,54 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int[] arr;
-	static String[] opArr;
-	static String[] opCalc;
-	static boolean[] path;
+	static int[] arr, op;
+	static int N;
 	
 	static int max = Integer.MIN_VALUE;
 	static int min = Integer.MAX_VALUE;
 		
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());	
+		N = Integer.parseInt(br.readLine());	
 		
-		arr = new int[n];
+		arr = new int[N];
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < n; i++) {
+		for(int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		int[] opCnt = new int[4];
 		st = new StringTokenizer(br.readLine());
+		op = new int[4];
 		for(int i = 0; i < 4; i++) {
-			opCnt[i] = Integer.parseInt(st.nextToken());
+			op[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		String[] opStr = {"+", "-", "*", "/"};
-		opArr = new String[n-1];		// 연산자 개수는 항상 입력숫자 -1
-		opCalc = new String[n-1];
-		path = new boolean[n-1];
-		int idx = 0;
-		
-		for(int i = 0; i < opCnt.length; i++) {
-			for(int j = 0; j < opCnt[i]; j++) {
-				opArr[idx++] = opStr[i];				
-			}
-		}
-		
-		acc(0, -1);
+	
+		dfs(arr[0], 1);
 		System.out.println(max);
 		System.out.println(min);
 	}
 	
-	static void acc(int deep, int e) {
-		if(deep == arr.length-1) {
-			int result = arr[0];
-			for(int i = 0; i < opCalc.length; i++) {
-				
-				switch (opCalc[i]) {
-				case "+":
-					result += arr[i+1];
-					break;
-				case "-":
-					result -= arr[i+1];
-					break;
-				case "*":
-					result *= arr[i+1];
-					break;
-				case "/":
-					result /= arr[i+1];
-					break;
-				}
-			}
-			
-			if(max < result)
-				max = result;
-			
-			if(min > result)
-				min = result;
+	static void dfs(int num, int idx) {
+		if(idx == N) {
+			max = Math.max(max, num);
+			min = Math.min(min, num);
 			return;
 		}
 		
-		for(int i = 0; i < opArr.length; i++) {
-			if(path[i])
+		for(int i = 0; i < 4; i++) {
+			if(op[i] <= 0)
 				continue;
 			
-			path[i] = true; 
-			opCalc[deep] = opArr[i];
-			acc(deep+1, 1);
-			path[i] = false;
+			op[i]--;
+			
+			switch (i) {
+			case 0: dfs(num + arr[idx], idx+1); break;
+			case 1: dfs(num - arr[idx], idx+1); break;
+			case 2: dfs(num * arr[idx], idx+1); break;
+			case 3: dfs(num / arr[idx], idx+1); break;
+			}
+			
+			op[i]++;
 		}
 	}
 }
