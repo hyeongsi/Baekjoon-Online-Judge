2a5a4b13892e279d20d739a87cca9b697468b962
박시형 <71202869+hyeongsi@users.noreply.github.com>
2023-07-28 23:23:17 +0900

    [Silver I] Title: 포도주 시식, Time: 152 ms, Memory: 16048 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -26,15 +26,12 @@ public class Main {
 			dp[2] = arr[1] + arr[2];
 		}
 		
-		System.out.println(recur(N));
-	}
-	
-	static int recur(int N) {
-		
-		if(dp[N] == null) {
-			dp[N] = Math.max(Math.max(recur(N-2), recur(N-3) + arr[N-1]) + arr[N], recur(N-1)); 
+		for(int i = 3; i <= N; i++) {
+			dp[i] = Math.max(Math.max(dp[i-2], dp[i-3] + arr[i-1]) + arr[i], dp[i-1]); 
 		}
 		
-		return dp[N];
+		System.out.println(dp[N]);
 	}
+	
+
 }
