fbb26b0e391a40114fb0d25912a59d4aaeea1664
박시형 <71202869+hyeongsi@users.noreply.github.com>
2023-08-12 13:12:02 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 888 ms, Memory: 118644 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -12,12 +12,12 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());	// 크기
 		int M = Integer.parseInt(st.nextToken());	// 합을 구해야 하는 횟수
 
-		int[][] arr = new int[N+1][N+1];
+		int[][] dp = new int[N+1][N+1];
 		
 		for(int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());	// N개의 수
 			for(int j = 1; j<= N; j++) {
-				arr[i][j] = arr[i][j-1] + Integer.parseInt(st.nextToken());				
+				dp[i][j] = dp[i][j-1] + dp[i-1][j] - dp[i-1][j-1] + Integer.parseInt(st.nextToken());				
 			}
 		}
 		
@@ -30,11 +30,8 @@ public class Main {
 			int x2 = Integer.parseInt(st.nextToken());
 			int y2 = Integer.parseInt(st.nextToken());
 			
-			long sum = 0;
-			for(int i = x1; i <= x2; i++) {
-				sum += arr[i][y2] - arr[i][y1-1];
-			}
-			sb.append(sum).append("\n");
+			int result = dp[x2][y2] - dp[x2][y1-1] - dp[x1-1][y2] + dp[x1-1][y1-1];
+			sb.append(result).append("\n");
 		}
 		
 		System.out.println(sb);
