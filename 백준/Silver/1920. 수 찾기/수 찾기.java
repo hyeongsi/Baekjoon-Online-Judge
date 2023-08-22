b70c25cf473a44571a290b440d18d5361559df9c
박시형 <71202869+hyeongsi@users.noreply.github.com>
2023-08-22 22:20:14 +0900

    [Silver IV] Title: 수 찾기, Time: 560 ms, Memory: 44224 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,34 +1,36 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {	
+	static int[] arr;
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();		
 		
-		int n = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		HashMap<Integer, Boolean> m = new HashMap<>();
-		for(int i = 0; i < n; i++) {
-			m.put(Integer.parseInt(st.nextToken()), false);
+		arr = new int[N];
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
+		Arrays.sort(arr);
 		
-		int k = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
 		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < k; i++) {
-			if(m.get(Integer.parseInt(st.nextToken())) != null)
-				bw.write(1 + "\n");
-			else
-				bw.write(0 + "\n");
+		for(int i = 0; i < M; i++) {
+			int v = Integer.parseInt(st.nextToken());
+			
+			if(Arrays.binarySearch(arr, v) < 0) {
+				sb.append("0\n");
+			}else{
+				sb.append("1\n");
+			}
 		}
 		
-		bw.flush();
-		bw.close();
-	}		
+		System.out.println(sb);
+	}
 }
