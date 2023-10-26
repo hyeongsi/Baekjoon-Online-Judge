fd46bf0af591b442af6b3fa45220c6121df57001
박시형 <71202869+hyeongsi@users.noreply.github.com>
2023-10-27 00:55:26 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long solution(long n) {
+        long sqrt = (long)Math.sqrt(n);
+        if(sqrt * sqrt == n){
+            sqrt++;
+            return sqrt * sqrt;
+        }
+        
+        return -1;
+    }
+}
