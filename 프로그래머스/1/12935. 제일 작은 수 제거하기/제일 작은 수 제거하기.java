3c568957cecff30a22f028d23dfbd5c79a24c8a5
박시형 <71202869+hyeongsi@users.noreply.github.com>
2023-11-08 22:43:01 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.10 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        
+        if(arr.length == 1){
+          int[] answer = {-1};
+          return answer;
+      }
+
+      int[] answer = new int[arr.length-1];
+      int minIndex=0;
+
+      for(int i=0;i<arr.length;i++){
+          if(arr[minIndex]>arr[i]){
+              minIndex = i;
+          }
+      }
+      for(int i=minIndex+1;i<arr.length;i++){
+          arr[i-1] = arr[i];
+      }
+      for(int i=0;i<answer.length;i++){
+          answer[i] = arr[i];
+      }
+      return answer;
+    }
+}
