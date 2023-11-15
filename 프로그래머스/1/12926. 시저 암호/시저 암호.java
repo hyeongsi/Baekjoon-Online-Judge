f214bbbf42e0296af785d32ff96f55ac686f3288
박시형 <71202869+hyeongsi@users.noreply.github.com>
2023-11-15 14:19:00 +0900

    [level 1] Title: 시저 암호, Time: 1.58 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -4,20 +4,15 @@ class Solution {
     public String solution(String s, int n) {
         StringBuilder sb = new StringBuilder();
         
+        n %= 26;
         for(int i = 0; i < s.length(); i++){
             char c = s.charAt(i);
             
-            if(c >= 'A' && c <= 'Z'){
-                c += n;
-                if(c > 'Z'){
-                    c = (char)('A' + (c - 'Z') - 1);
-                }
-            } else if(c >= 'a' && c <= 'z'){
-                c += n;
-                if(c > 'z'){
-                    c = (char)('a' + (c - 'z') - 1);
-                }
-            } 
+            if(Character.isLowerCase(c)){
+                c = (char)((c - 'a' + n) % 26 + 'a');
+            }else if(Character.isUpperCase(c)){
+                c = (char)((c - 'A' + n) % 26 + 'A');
+            }
             
             sb.append(c);
         }
