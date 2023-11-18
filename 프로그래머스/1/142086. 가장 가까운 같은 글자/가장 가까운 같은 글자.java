62abb80ef21b34d606489200de2a71643a0974f5
박시형 <71202869+hyeongsi@users.noreply.github.com>
2023-11-18 22:08:45 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 2.51 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -8,11 +8,7 @@ class Solution {
         for(int i = 0; i < s.length(); i++){
             char ch = s.charAt(i);
             
-            if(map.containsKey(ch)){
-                answer[i] = i - map.get(ch);
-            }else{
-                answer[i] = -1;
-            }
+            answer[i] = i - map.getOrDefault(ch, i+1);
             map.put(ch, i);
         }
         
