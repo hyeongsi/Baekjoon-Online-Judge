e4b9b10629de59a88429a7fe12019feeaafc6af4
박시형 <71202869+hyeongsi@users.noreply.github.com>
2023-11-29 18:09:21 +0900

    [level 1] Title: 옹알이 (2), Time: 3.10 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -3,34 +3,12 @@ import java.util.*;
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
-        String[] words = {"aya", "ye", "woo", "ma"};
-        
-        for(int i = 0; i < babbling.length; i++){
-            StringBuilder sb = new StringBuilder();
-            String prev = "";
-            boolean isPass = false;
-            for(int j = 0; j < babbling[i].length(); j++) {
-                sb.append(babbling[i].charAt(j));
-                
-                if(sb.toString().equals(prev)){
-                    isPass = false;
-                    break;
-                }
-                
-                for(String word : words){
-                    if(sb.toString().equals(word)) {
-                        isPass = true;
-                        prev = sb.toString();
-                        sb = new StringBuilder();
-                        break;
-                    }
-                } 
-            }
-            if(sb.length() == 0){
-                answer++;
-            }
+        for(int i=0; i<babbling.length; i++){
+            babbling[i] = babbling[i].replaceAll("ayaaya|yeye|woowoo|mama","1");
+            babbling[i] = babbling[i].replaceAll("aya|ye|woo|ma","");
+            // babbling[i] = babbling[i].replaceAll(" ","");
+            if(babbling[i].equals("")) answer++;
         }
-        
         return answer;
     }
 }
