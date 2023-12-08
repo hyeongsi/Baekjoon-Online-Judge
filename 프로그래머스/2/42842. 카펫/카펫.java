3de9a91d80d0c1c46dacace3402a1707f2211095
박시형 <71202869+hyeongsi@users.noreply.github.com>
2023-12-08 22:18:38 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        for(int i=1; i<=yellow; i++) {
+            if(yellow%i==0 && (yellow/i+i)*2+4==brown) {
+                return new int[] {yellow/i+2, i+2};
+            }
+        }
+        return null;
+    }
+}
