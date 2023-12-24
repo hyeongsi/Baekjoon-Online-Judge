896d7ebffa3be94e41fbd8808e82f0d6dff29314
박시형 <71202869+hyeongsi@users.noreply.github.com>
2023-12-24 23:36:24 +0900

    [level 3] Title: 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/README.md
+++ 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/README.md
@@ -1,6 +1,6 @@
 # [level 3] 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기 - 151139 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/151139?language=oracle) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/151139) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2023년 11월 0일 22:3:24
+2023년 12월 0일 23:36:19
 
 ### 문제 설명
 
--- 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
+++ 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
@@ -1,21 +1,15 @@
-select to_number(month) as month,car_id,records from (
-select to_char(a.start_date,'mm') as month
-     , a.car_id
-     , count(*) as records
-from car_rental_company_rental_history a,(
-    --5회이상인 car_id 추출
-    select car_id from car_rental_company_rental_history
-    where to_char(start_date,'yyyymm')>='202208'
-      and to_char(start_date,'yyyymm')<='202210'
-      group by car_id
-      having count(*) >=5
-    ) b
-where a.car_id = b.car_id
-  and to_char(a.start_date,'yyyymm')>='202208'
-  and to_char(a.start_date,'yyyymm')<='202210'
-
-group by to_char(a.start_date,'mm')
-     , a.car_id
-having count(*) > 0
-    )
-order by month,car_id desc
+SELECT  TO_CHAR(START_DATE, 'mm')*1 MONTH, CAR_ID,  COUNT(*) RECORDS
+FROM    CAR_RENTAL_COMPANY_RENTAL_HISTORY A
+WHERE   1=1
+        AND TO_CHAR(START_DATE,'YYYYMM') BETWEEN '202208' AND '202210'
+        AND CAR_ID IN   (
+                        SELECT  CAR_ID
+                        FROM    CAR_RENTAL_COMPANY_RENTAL_HISTORY A
+                        WHERE   1=1
+                                AND TO_CHAR(START_DATE,'YYYYMM') BETWEEN '202208' AND '202210'
+                        GROUP BY CAR_ID
+                        HAVING   COUNT(*) > 4
+                        )
+GROUP BY    TO_CHAR(START_DATE, 'mm'), CAR_ID
+HAVING      COUNT(*) <> 0
+ORDER BY    1 ASC, 2 DESC
