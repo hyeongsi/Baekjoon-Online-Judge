b316054a583cdebe2343aa2f5852c216ea306c42
박시형 <71202869+hyeongsi@users.noreply.github.com>
2024-02-25 22:33:57 +0900

    [level 2] Title: 행렬의 곱셈, Time: 1.60 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -1,18 +1,20 @@
 import java.util.*;
 
 class Solution {
-    public int[][] solution(int[][] arr1, int[][] arr2) {
-        
-        int[][] answer = new int[arr1.length][arr2[0].length];
+    public int[][] solution(int[][] A, int[][] B) {
+        int lX = A.length;
+        int lY = B[0].length;
+        int[][] answer = new int[lX][lY];
 
-        for (int i = 0; i < answer.length; i++) {
-            for (int j = 0; j < answer[0].length; j++) {
-                for (int k = 0; k < arr1[0].length; k++) {
-                    answer[i][j] += arr1[i][k] * arr2[k][j];
+        for (int i = 0; i < lX; i++) {
+            for (int j = 0; j < lY; j++) {
+                int value = 0;
+                for (int k = 0; k < A[0].length; k++) {
+                    value += A[i][k] * B[k][j];
                 }
+                answer[i][j] = value;
             }
         }
-        
         return answer;
     }
 }
