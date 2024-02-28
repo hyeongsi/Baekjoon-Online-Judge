2ab8b7975939f24844d918cff5ffd988b1569a7e
박시형 <71202869+hyeongsi@users.noreply.github.com>
2024-02-28 21:34:05 +0900

    [level 2] Title: 튜플, Time: 1.03 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -2,35 +2,17 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String s) {
+        Set<String> set = new HashSet<>();
+        String[] arr = s.replaceAll("[{]", " ").replaceAll("[}]", " ").trim().split(" , ");
+        Arrays.sort(arr, (a, b)->{return a.length() - b.length();});
         
-        Map<Integer, Integer> map = new HashMap<>();
-        String numStr = "";
-        for (int i = 1; i < s.length() - 1; i++) {
-            final char ch = s.charAt(i);
-            
-            switch (ch) {
-                case '{':
-                    numStr = "";
-                    break;
-                case ',':
-                case '}': 
-                    if (numStr.equals(""))
-                        break;
-                    
-                    final int key = Integer.parseInt(numStr);
-                    map.put(key, map.getOrDefault(key, 0) + 1);
-                    numStr = "";
-                    break;  
-                default: numStr += ch; break;
+        int[] answer = new int[arr.length];
+        int idx = 0;
+        for(String s1 : arr) {
+            for(String s2 : s1.split(",")) {
+                if(set.add(s2)) answer[idx++] = Integer.parseInt(s2);
             }
         }
-        
-        int[] answer = new int[map.size()];
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            final int idx = map.size() - entry.getValue();
-            answer[idx] = entry.getKey();
-        }
-        
         return answer;
     }
 }
