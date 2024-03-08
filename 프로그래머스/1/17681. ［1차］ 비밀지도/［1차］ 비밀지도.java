2c7a556e65c443ccdd03718beba5b48bdcb479cd
박시형 <71202869+hyeongsi@users.noreply.github.com>
2024-03-08 18:02:18 +0900

    [level 1] Title: [1차] 비밀지도, Time: 10.01 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,5 +1,5 @@
 class Solution {
-    public String[] solution(int n, int[] arr1, int[] arr2) {
+  public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] result = new String[n];
         for (int i = 0; i < n; i++) {
             result[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
