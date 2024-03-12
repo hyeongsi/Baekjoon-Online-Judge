75f29d9fa1e42289b1a77e6005f50949d9080c01
박시형 <71202869+hyeongsi@users.noreply.github.com>
2024-03-12 18:21:12 +0900

    [level 2] Title: [3차] 압축, Time: 11.15 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -1,58 +1,42 @@
 import java.util.*;
 
 class Solution {
-  public int[] solution(String msg) {
-        int nextIndex = 27;
-        int nextStartIndex = 0;
-
-        Map<String, Integer> dictionary = new HashMap<>();
-        List<Integer> list = new ArrayList<>();
-
+    public int[] solution(String msg) {
+        Map<String, Integer> map = new HashMap<>();
+        List<Integer> ans = new ArrayList<>();
+        
+        for (int i = 0; i <= 'Z' - 'A' ; i++) {
+            String str = "" + (char)(i + 'A');
+            map.put(str, i + 1);
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        int idx = 0;
         while (true) {
-            if (nextStartIndex > msg.length() - 1) { 
-                break;
-            }
-            if (nextStartIndex == msg.length() - 1) { 
-                char w = msg.charAt(nextStartIndex);
-                list.add(w - 64);
-                break;
-            }
-
-            String toFind = msg.substring(nextStartIndex, nextStartIndex + 2);
-            String w = "";
-            if (dictionary.containsKey(toFind)) {
-                w = toFind;
-                boolean findLargerW = true;
-                int i = 2;
-                while (findLargerW) {
-                    i++;
-                    if (nextStartIndex + i > msg.length()) {
-                        break;
-                    }
-                    toFind = msg.substring(nextStartIndex, nextStartIndex + i);
-                    if (dictionary.containsKey(toFind)) {
-                        findLargerW = true;
-                        w = toFind;
-                    } else {
-                        findLargerW = false;
-                    }
+            sb.append(msg.charAt(idx));
+            
+            if (map.containsKey(sb.toString())) {
+                if (idx == msg.length() - 1) {
+                    ans.add(map.get(sb.toString()));
+                    break;
                 }
-                list.add(dictionary.get(w));
-                dictionary.put(toFind, nextIndex++);
-                nextStartIndex += w.length();
-
-            } else {
-                char c = msg.charAt(nextStartIndex);
-                list.add(c - 64);
-                dictionary.put(toFind, nextIndex++);
-                nextStartIndex++;
+                
+                idx++;
             }
+            else {
+                map.put(sb.toString(), map.size() + 1);
+                sb.deleteCharAt(sb.length() - 1);
+                ans.add(map.get(sb.toString()));
+                sb.setLength(0);
+            }
+            
         }
-
-        int[] answer = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
+        
+        int[] answer = new int[ans.size()];
+        for (int i = 0; i < ans.size(); i++) {
+            answer[i] = ans.get(i);
         }
+        
         return answer;
     }
 }
