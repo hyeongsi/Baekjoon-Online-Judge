a2540272ba74536bcbf54f7d8affdb8fdf2fe11b
박시형 <71202869+hyeongsi@users.noreply.github.com>
2024-04-08 19:32:34 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.24 ms, Memory: 52.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -4,30 +4,32 @@ class Solution {
     public int solution(int[][] maps) {
         int rows = maps.length;
         int cols = maps[0].length;
-    
+        
         int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
         
         Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{0, 0, 1});    // 시작 위치와 거리
+        queue.offer(new int[]{0, 0, 1}); // x, y, 길이
         
         while (!queue.isEmpty()) {
-            int[] current = queue.poll();
-            int row = current[0];
-            int col = current[1];
-            int distance = current[2];
+            int[] pos = queue.poll();
+            int row = pos[0];
+            int col = pos[1];
+            int distance = pos[2];
             
             if (row == rows - 1 && col == cols - 1) {
-                return distance;    // bfs는 가장 먼저 도달한게 최단거리
+                return distance;
             }
             
             for (int[] dir : directions) {
                 int newRow = row + dir[0];
                 int newCol = col + dir[1];
                 
-                if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols && maps[newRow][newCol] == 1) {
+                if (  newRow >= 0 && newRow < rows
+                   && newCol >= 0 && newCol < cols
+                   && maps[newRow][newCol] == 1) {
                     maps[newRow][newCol] = 0;
-                    queue.offer(new int[]{newRow, newCol, distance + 1});
-                }
+                    queue.add(new int[] {newRow, newCol, distance + 1});
+                } 
             }
         }
         
