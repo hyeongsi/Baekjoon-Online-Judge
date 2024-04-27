d63a8d1db5060008cf9887e8e139db4abff3a02d
박시형 <71202869+hyeongsi@users.noreply.github.com>
2024-04-27 13:50:34 +0900

    [Silver II] Title: 색종이 만들기, Time: 152 ms, Memory: 15620 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -4,62 +4,51 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int[][] arr;
-	static int white = 0;
-	static int blue = 0;
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		arr = new int[N][N];
-		for(int y = 0; y < N; y++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for(int x = 0; x < N; x++) {
-				arr[y][x] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		divide(0, 0, N);
-		System.out.println(white);
-		System.out.println(blue);
-	}
-	
-	static void divide(int iy, int ix, int div) {
-		boolean isdivide = false;
-		
-		int prevValue = arr[iy][ix];
-		if(div == 1) {
-			cnt(prevValue);
-			return;
-		}
-		
-		escape:
-		for(int y = iy; y < iy+div; y++) {
-			for(int x = ix; x < ix+div; x++) {
-				if(prevValue != arr[y][x]) {
-					isdivide = true;
-					break escape;
-				}
-			}
-		}
-		
-		if(isdivide) {
-			int half = div/2;
-			
-			divide(iy, ix, half);
-			divide(iy+half, ix, half);
-			divide(iy, ix+half, half);
-			divide(iy+half, ix+half, half);
-		}else {
-			cnt(prevValue);
-		}
-	}
-	
-	static void cnt(int value) {
-		if(value == 0)
-			white++;
-		else
-			blue++;
-	}
+    static int white = 0;
+    static int blue = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Integer paperSize = Integer.parseInt(br.readLine());
+
+        String[][] paper = new String[paperSize][paperSize];
+        for (int i = 0; i < paperSize; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < paperSize; j++) {
+                paper[i][j] = st.nextToken();
+            }
+        }
+
+        CountColor(paper, paperSize, 0, 0);
+
+        System.out.println(white);
+        System.out.println(blue);
+
+    }// end main
+
+    public static void CountColor(String[][] paper, int checkSize, int x, int y) {
+        final int ckHalf = checkSize / 2;
+        final String firstColor = paper[y][x];
+
+        for (int i = y; i < y + checkSize; i++) {
+            for (int j = x; j < x + checkSize; j++) {
+                if (!firstColor.equals(paper[i][j])) {
+                    CountColor(paper, ckHalf, x, y);
+                    CountColor(paper, ckHalf, x + ckHalf, y);
+                    CountColor(paper, ckHalf, x, y + ckHalf);
+                    CountColor(paper, ckHalf, x + ckHalf, y + ckHalf);
+                    return;
+                }
+            }
+        }
+
+        if (firstColor.equals("1")) {
+            blue++;
+        } else {
+            white++;
+        }
+
+    }
+
 }
+
