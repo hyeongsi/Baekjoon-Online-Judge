9a9a973b2e7287738061ac93fb24ff24d4796f03
박시형 <71202869+hyeongsi@users.noreply.github.com>
2024-05-01 23:33:25 +0900

    [level 2] Title: 튜플, Time: 1.01 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/README.md
+++ 프로그래머스/2/64065. 튜플/README.md
@@ -1,10 +1,10 @@
 # [level 2] 튜플 - 64065 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065) 
 
 ### 성능 요약
 
-메모리: 72.5 MB, 시간: 1.03 ms
+메모리: 77.8 MB, 시간: 1.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 02월 28일 21:34:03
+2024년 05월 01일 23:33:20
 
 ### 문제 설명
 
--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,11 +1,9 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(String s) {
         Set<String> set = new HashSet<>();
         String[] arr = s.replaceAll("[{]", " ").replaceAll("[}]", " ").trim().split(" , ");
         Arrays.sort(arr, (a, b)->{return a.length() - b.length();});
-        
         int[] answer = new int[arr.length];
         int idx = 0;
         for(String s1 : arr) {
