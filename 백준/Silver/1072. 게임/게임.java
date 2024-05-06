2538bd768fea48df519dc2e4539e6da997195f1e
박시형 <71202869+hyeongsi@users.noreply.github.com>
2024-05-06 17:37:14 +0900

    [Silver III] Title: 게임, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Silver/1072. 게임/게임.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int z = calcPercent(x, y);
+
+        int ans = -1;
+        int left = 0;
+        int right = (int) 1e9;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (calcPercent(x + mid, y + mid) != z) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        System.out.println(ans);
+
+    }// end main
+
+    static int calcPercent(int x, int y) {
+        return (int) ((long) y * 100 / x);
+    }
+}
+
