525cfab4149d449eb7df53e8e1b5d0c9bc7bdd1a
박시형 <71202869+hyeongsi@users.noreply.github.com>
2024-05-10 23:56:15 +0900

    [level 2] Title: 더 맵게, Time: 1509.30 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,27 +1,18 @@
-import java.util.*;
+import java.util.PriorityQueue;
 
 class Solution {
     public int solution(int[] scoville, int K) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        
-        for (int scov : scoville) {
-            pq.add(scov);
-        }
-          
-        int answer = 0;
-        while (pq.size() > 1 && pq.peek() < K) {
-            int weakHot = pq.poll();
-            int secondWeakHot = pq.poll();
-            
-            int mixHot = weakHot + (secondWeakHot * 2);
-            pq.add(mixHot);
-            answer++;
-        }
-        
-        if (pq.size() <= 1 && pq.peek() < K) {
-            answer = -1;
-        }
-        
-        return answer;
+         PriorityQueue<Integer> pqScov = new PriorityQueue<>();
+         for (int s: scoville) {
+             pqScov.add(s);
+         }
+
+         int cnt = 0;
+         while (pqScov.size() > 1 && pqScov.peek() < K) {
+             pqScov.add(pqScov.remove() + pqScov.remove() * 2);
+             cnt++;
+         }
+
+         return pqScov.peek() >= K ? cnt : -1;
     }
 }
