ae8d003c5bde1fc1c35b02d79ae98244b002a9c8
박시형 <71202869+hyeongsi@users.noreply.github.com>
2025-02-22 22:33:29 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 100 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int i = Integer.parseInt(br.readLine());
+        int diffNum = 2541-1998;
+
+        System.out.println(i - diffNum);
+    }
+}
