17cb0c6bb59e8e53de9e85a9503432821085835b
박시형 <71202869+hyeongsi@users.noreply.github.com>
2025-02-25 22:34:43 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 124 ms, Memory: 16060 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -2,37 +2,39 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		
-		int[] bktArr = new int[n];
-		Arrays.setAll(bktArr, num->num+1);
-		
-		int bkt_i, bkt_j;
-		for(int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-			bkt_i = Integer.parseInt(st.nextToken()) - 1;
-			bkt_j = Integer.parseInt(st.nextToken()) - 1;
-			
-			int temp;
-			for(int j = 0; j < (bkt_j - bkt_i) / 2f; j++) {
-				temp = bktArr[bkt_i + j];
-				bktArr[bkt_i + j] = bktArr[bkt_j - j];
-				bktArr[bkt_j - j] = temp;
-			}
-		}
-		br.close();
-		
-		for(int i : bktArr)
-			System.out.print(i + " ");
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        final int N = Integer.parseInt(st.nextToken()); // 바구니 수
+        final int M = Integer.parseInt(st.nextToken()); // 반복 수
+
+        final int[] baskets = new int[N];
+        Arrays.setAll(baskets, i -> i + 1);
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int i1 = Integer.parseInt(st.nextToken());
+            int i2 = Integer.parseInt(st.nextToken());
+
+            while(i1 < i2) {
+                reverseBasketIndex(baskets, i1, i2);
+                i1++;
+                i2--;
+            }
+        }
+
+        for (int basket : baskets) {
+            System.out.print(basket + " ");
+        }
+    }
+
+    static void reverseBasketIndex(int[] baskets, int i, int j) {
+        int temp = baskets[i - 1];
+        baskets[i - 1] = baskets[j - 1];
+        baskets[j - 1] = temp;
+    }
 }
