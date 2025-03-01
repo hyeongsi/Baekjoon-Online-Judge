280eccbe993c1a1a756334113b8e8635a98bfb97
박시형 <71202869+hyeongsi@users.noreply.github.com>
2025-03-01 23:00:23 +0900

    [Bronze I] Title: 단어 공부, Time: 200 ms, Memory: 13060 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.js
+++ 백준/Bronze/1157. 단어 공부/단어 공부.js
@@ -1,22 +1,11 @@
 const fs = require('fs');
 const input = fs.readFileSync('./dev/stdin').toString().trim();
 
-const alpahbet = new Map();
-let max = 0;
-for (let i = 0; i < input.length; i++) {
-    const key = input[i].toUpperCase();
-    const value = alpahbet.get(key) + 1 || 1;
-    alpahbet.set(key, value);
-
-    if (value > max)
-        max = value;
+const word = input.toUpperCase();
+const hash = {};
+for (let i = 0; i < word.length; i++) {
+    hash[word[i]] = (hash[word[i]] || 0) + 1;
 }
-
-let result = "";
-alpahbet.forEach((value, key) => {
-    if (max === value) {
-        result = result != "" ? "?" : key;
-    }
-});
-
-console.log(result);
+const max = Math.max(...Object.values(hash));
+const ans = Object.keys(hash).filter(v => hash[v] == max );
+console.log(ans.length > 1 ? '?' : ans[0]);
