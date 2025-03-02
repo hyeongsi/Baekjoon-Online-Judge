291474beba0904b16f404c7eec20466d1c9681ab
박시형 <71202869+hyeongsi@users.noreply.github.com>
2025-03-02 16:15:30 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 112 ms, Memory: 14560 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,42 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int loop = Integer.parseInt(br.readLine());
-		
-		String[] str = new String[loop];
-		for(int i = 0; i < loop; i++) {
-			str[i] = br.readLine();
-		}
-		
-		int result = 0;
-		int prev = -1;
-		
-		for(int i = 0; i < str.length; i++) {
-			boolean[] alphabet = new boolean[26];
-			
-			for(int j = 0; j < str[i].length(); j++) {
-				if(alphabet[str[i].charAt(j)-'a'] == false) {
-					alphabet[str[i].charAt(j)-'a'] = true;
-					prev = str[i].charAt(j)-'a';
-				}
-				else if(alphabet[str[i].charAt(j)-'a'] == true){
-					if(prev != str[i].charAt(j)-'a') {
-						prev = -1;
-						break;
-					}
-				}
-			}
-			
-			if(prev != -1)
-				result++;
-		}
-		
-		System.out.println(result);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int loop = Integer.parseInt(br.readLine());
+
+        int dupCnt = 0;
+        for (int i = 0; i < loop; i++) {
+            String[] input = br.readLine().split("");
+
+            Map<String, Boolean> hashMap = new HashMap<>();
+            String prev = input[0];
+
+            for (int j = 0; j < input.length; j++) {
+                String key = input[j];
+
+                if (!hashMap.containsKey(key)) {
+                    hashMap.put(key, true);
+                } else if (!prev.equals(key)) {
+                    dupCnt++;
+                    break;
+                }
+
+                prev = key;
+            }
+        }
+
+        System.out.println(loop - dupCnt);
+    }
 }
