b11400ffe3ecdc90776540ec8d79f9b70dcb1562
박시형 <71202869+hyeongsi@users.noreply.github.com>
2025-03-03 13:23:05 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 296 ms, Memory: 19360 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -1,38 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
+import java.util.stream.IntStream;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String[] size = br.readLine().split(" ");
-		int n = Integer.parseInt(size[0]);
-		int m = Integer.parseInt(size[1]);
-		
-		int[][] matrix = new int[n][m];
-		
-		for(int k = 0; k < 2; k++) {
-			for(int i = 0; i < n; i++) {
-				StringTokenizer st = new StringTokenizer(br.readLine());
-				for(int j = 0; j < m; j++) {
-					int value = Integer.parseInt(st.nextToken());
-					
-					if(k == 0)
-						matrix[i][j] = value;
-					else
-						matrix[i][j] = matrix[i][j] + value;
-				}
-			}
-		}
-		
-		for(int[] e:matrix) {
-			for(int f:e) {
-				System.out.print(f + " ");
-			}
-			System.out.println("");
-		}
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] matrix = new int[n][m];
+
+        for (int i = 0; i < 2; i++) {
+            for (int y = 0; y < n; y++) {
+                int[] array = getIntArrToString(br.readLine());
+                sumArrayElement(matrix[y], array);
+            }
+        }
+
+        for (int[] line : matrix) {
+            for (int e : line) {
+                System.out.print(e + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[] getIntArrToString(String str) {
+        return Arrays.stream(str.split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+    }
+
+    public static void sumArrayElement(int[] a, int[] b) {
+        IntStream.range(0, Math.min(a.length, b.length))
+                .forEach(i -> a[i] += b[i]);
+    }
 }
