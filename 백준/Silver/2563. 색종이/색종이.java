dc30b7afc6a1fd335fd4874fccccdb9a1d3e49db
박시형 <71202869+hyeongsi@users.noreply.github.com>
2025-03-09 19:04:10 +0900

    [Silver V] Title: 색종이, Time: 104 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,37 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int loop = Integer.parseInt(br.readLine());
-		boolean[][] paper = new boolean[100][100];
-		
-		// 입력
-		for(int i = 0; i < loop; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			
-			int y = Integer.parseInt(st.nextToken());
-			int x = Integer.parseInt(st.nextToken());
-			
-			for(int j = y; (j <= 100) && (j < y + 10); j++) {
-				for(int k = x; (k <= 100) && (k < x + 10); k++) {
-					paper[j-1][k-1] = true;
-				}
-			}
-		}
-		
-		int sum = 0;
-		for(int i = 0; i < 100; i++) {
-			for(int j = 0; j < 100; j++) {
-				if(paper[i][j])
-					sum++;
-			}
-		}
-		
-		System.out.println(sum);
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int loop = Integer.parseInt(br.readLine());
+
+        int ans = 0;
+        boolean[][] paper = new boolean[100][100];
+        for (int i = 0; i < loop; i++) {
+            String[] line = br.readLine().split(" ");
+            int x = Integer.parseInt(line[0]);
+            int y = Integer.parseInt(line[1]);
+
+            for (int j = x; j < x + 10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    if (!paper[j][k]) {
+                        paper[j][k] = true;
+                        ans++;
+                    }
+                }
+            }
+        }
+
+        System.out.println(ans);
+    }
+
 }
