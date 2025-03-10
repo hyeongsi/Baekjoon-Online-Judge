bc19d39810a74952b165818681800f30a079796f
박시형 <71202869+hyeongsi@users.noreply.github.com>
2025-03-10 19:32:03 +0900

    [Bronze I] Title: 진법 변환 2, Time: 108 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -1,35 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int num = Integer.parseInt(st.nextToken());
-		int base = Integer.parseInt(st.nextToken());
-		
-		String result = "";
-		
-		int quot = 0, remain = 0;
-		while(num > 0) {
-			quot = num / base;
-			remain = num % base;
-			
-			num = quot;
-			if(remain < 10) { // 숫자
-				result += Integer.toString(remain);
-			}
-			else{ // 알파벳
-				result += Character.toString((remain + 'A' - 10));
-			}
-		}
-		
-		for(int i = result.length()-1; i >= 0; i--) {
-			System.out.print(result.charAt(i));
-		}
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split(" ");
+
+        final int base = Integer.parseInt(inputs[1]);
+
+        System.out.println(Integer.toString(Integer.parseInt(inputs[0]), base).toUpperCase());
+    }
+
 }
