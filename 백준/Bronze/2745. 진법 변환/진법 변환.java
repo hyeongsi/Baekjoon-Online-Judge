82b980a13342daf37eb4e3c6cda28b7dd4d051f2
박시형 <71202869+hyeongsi@users.noreply.github.com>
2025-03-10 19:06:08 +0900

    [Bronze II] Title: 진법 변환, Time: 108 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.java
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -1,32 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		String str = st.nextToken();
-		int base = Integer.parseInt(st.nextToken());
-		
-		int result = 0;
-		for(int i = str.length()-1; i >=0; i--) {
-			char ch = str.charAt(i);
-			int temp;
-			
-			if(Character.isAlphabetic(ch)) {
-				temp = ch - 'A' + 10;
-			}
-			else {
-				temp = ch - '0';
-			}
 
-			result += temp * Math.pow(base, str.length()-1 - i);
-		}
-		
-		System.out.println(result);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split(" ");
+
+        final int base = Integer.parseInt(inputs[1]);
+
+        System.out.println(Integer.parseInt(inputs[0], base));
+    }
+
 }
