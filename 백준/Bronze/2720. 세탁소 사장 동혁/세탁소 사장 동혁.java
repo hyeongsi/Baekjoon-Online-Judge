f8cc6036f8392da78215249151ad01920649530c
박시형 <71202869+hyeongsi@users.noreply.github.com>
2025-03-11 22:24:18 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 124 ms, Memory: 14792 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -3,20 +3,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int[] changes = {25, 10, 5, 1};
-		
-		int loop = Integer.parseInt(br.readLine());
-		for(int i = 0; i < loop; i++) {
-			int change = Integer.parseInt(br.readLine());
-			
-			for(int j = 0; j < changes.length; j++) {
-				System.out.print(change / changes[j] + " ");
-				change = change % changes[j];
-			}
-			System.out.println();
-		}
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final int tCnt = Integer.parseInt(br.readLine());
+
+        int[] coins = {25, 10, 5, 1};
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < tCnt; i++) {
+            int change = Integer.parseInt(br.readLine());
+
+            for (int j = 0; j < coins.length; j++) {
+                sb.append(change / coins[j] + " ");
+                change %= coins[j];
+            }
+
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
 }
