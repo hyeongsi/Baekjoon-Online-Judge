9167775b213723d1f0342902c2ae0f61b52d2b3a
박시형 <71202869+hyeongsi@users.noreply.github.com>
2025-03-12 21:06:08 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 104 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
@@ -3,18 +3,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int num = Integer.parseInt(br.readLine());
 
-		int dot = 2;
-		int addRatio = 0;
-		for(int i = 0; i < num; i ++) {
-			addRatio = (int)Math.pow(2, i);
-			dot += addRatio;
-		}
-		
-		System.out.println(dot * dot);		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int i = Integer.parseInt(br.readLine());
+
+        int sum = 2;
+        for (int j = 0; j < i; j++) {
+            sum += Math.pow(2, j);
+        }
+        System.out.println(sum * sum);
+
+    }
+
 }
