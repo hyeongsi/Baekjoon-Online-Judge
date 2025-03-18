c1c3f0231e96cbbdbe690a16cb36bd6cf5679cf7
박시형 <71202869+hyeongsi@users.noreply.github.com>
2025-03-18 20:02:19 +0900

    [Bronze III] Title: 배수와 약수, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Bronze/5086. 배수와 약수/배수와 약수.java
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -1,34 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st;
-		
-		String[] stateStr = {"neither", "factor", "multiple"}; // x, 약수, 배수
-		int stateIndex;
-		
-		while(true) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			
-			stateIndex = 0;
-			
-			if(a == 0 && b == 0)
-				break;
-			
-			if((a <= b) && (b % a == 0)) {
-				stateIndex = 1;
-			}
-			else if ((a > b) && ((a % b == 0)))
-				stateIndex = 2;
-			
-			System.out.println(stateStr[stateIndex]);
-		}
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+
+        while (true) {
+            int[] array = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            if (array[0] == 0 && array[1] == 0) break;
+
+            if (array[0] % array[1] == 0) {
+                sb.append("multiple\n");
+            } else if (array[1] % array[0] == 0) {
+                sb.append("factor\n");
+            } else {
+                sb.append("neither\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
 }
