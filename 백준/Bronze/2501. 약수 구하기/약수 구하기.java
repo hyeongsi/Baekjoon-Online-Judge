b9f473b979e023f72b2080d287a566992c2a7983
박시형 <71202869+hyeongsi@users.noreply.github.com>
2025-03-19 20:57:55 +0900

    [Bronze III] Title: 약수 구하기, Time: 108 ms, Memory: 14592 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.java
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -1,30 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.TreeSet;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(st.nextToken());
-		
-		int cnt = 0;
-		int msr = 0;
-		for(int i = 1; i <= n; i++ ) {
-			if(n % i == 0)
-				cnt++;
-			
-			if(cnt == k) {
-				msr = i;
-				break;
-			}
-		}
-		
-		System.out.println(msr);
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] inputs = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        TreeSet<Integer> treeSet = new TreeSet<>();
+        for (int i = 1; i <= (int)Math.sqrt(inputs[0]); i++) {
+            if (inputs[0] % i == 0) {
+                treeSet.add(i);
+                treeSet.add(inputs[0] / i);
+            }
+        }
+        if (treeSet.size() < inputs[1]) {
+            System.out.println(0);
+        } else {
+            for (int i = 0; i < inputs[1] - 1; i++) {
+                treeSet.pollFirst();
+            }
+            System.out.println(treeSet.pollFirst());
+        }
+    }
 }
