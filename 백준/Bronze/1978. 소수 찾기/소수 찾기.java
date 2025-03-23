f70d61b169d5c3fdf61fe7f9d568fd3604d83885
박시형 <71202869+hyeongsi@users.noreply.github.com>
2025-03-23 13:19:55 +0900

    [Bronze II] Title: 소수 찾기, Time: 100 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -1,32 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-				
-		int l = Integer.parseInt(br.readLine());
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int result = 0;
-		for(int i = 0; i < l; i++) {
-			int n = Integer.parseInt(st.nextToken());
-			
-			boolean decimal = true;
-			for(int j = 2; j <= Math.sqrt(n); j++) {
-				if(n % j == 0) {
-					decimal = false;
-					break;
-				}
-			}
-			
-			if(decimal && n != 1)
-				result++;
-		}
-		
-		System.out.println(result);
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] nums = br.readLine().split(" ");
+
+        boolean[] prime = new boolean[1001];
+        prime[0] = prime[1] = true;
+        for (int i = 2; i <= Math.sqrt(1000); i++) {
+
+            if (prime[i] == true) continue;
+
+            for (int j = i * i; j < prime.length; j += i) {
+                prime[j] = true;
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (!prime[Integer.parseInt(nums[i])]) result ++;
+        }
+
+        System.out.println(result);
+    }
 }
