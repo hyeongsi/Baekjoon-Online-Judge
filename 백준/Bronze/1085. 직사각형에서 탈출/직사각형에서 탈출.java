723c00b049ff04943312cea3ef98b3e2c9bf8e7c
박시형 <71202869+hyeongsi@users.noreply.github.com>
2025-04-09 21:30:41 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 108 ms, Memory: 14428 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
@@ -1,22 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int x = Integer.parseInt(st.nextToken());
-		int y = Integer.parseInt(st.nextToken());
-		
-		int w = Integer.parseInt(st.nextToken());
-		int h = Integer.parseInt(st.nextToken());
-		
-		int min = Math.min(Math.min(w-x, x), Math.min(h-y, y));
-		
-		System.out.println(min);
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int min = Integer.MAX_VALUE;
+        min = Math.min(min, array[0]);
+        min = Math.min(min, Math.abs(array[0] - array[2]));
+        min = Math.min(min, array[1]);
+        min = Math.min(min, Math.abs(array[1] - array[3]));
+
+        System.out.println(min);
+    }
 }
