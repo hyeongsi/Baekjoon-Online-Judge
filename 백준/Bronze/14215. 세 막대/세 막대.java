5abcfe3120ae96c027b57d43bb1e1b95b82a493b
박시형 <71202869+hyeongsi@users.noreply.github.com>
2025-06-28 10:03:58 +0900

    [Bronze III] Title: 세 막대, Time: 116 ms, Memory: 14544 KB -BaekjoonHub

--- 백준/Bronze/14215. 세 막대/세 막대.java
+++ 백준/Bronze/14215. 세 막대/세 막대.java
@@ -2,25 +2,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int[] n = new int[3];
-		
-		n[0] = Integer.parseInt(st.nextToken());
-		n[1] = Integer.parseInt(st.nextToken());
-		n[2] = Integer.parseInt(st.nextToken());
-		
-		Arrays.sort(n);
-		
-		if(n[0] + n[1] <= n[2]) {
-			n[2] = n[0] + n[1] - 1;
-		}
-		
-		System.out.println(n[0] + n[1] + n[2]);
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> sides = Arrays.stream(br.readLine().split(" ")).map(Integer::parseInt).sorted().collect(Collectors.toList());
+
+        if (sides.get(0) + sides.get(1) <= sides.get(2)) {
+            System.out.println((sides.get(0) + sides.get(1)) * 2 - 1);
+        } else {
+            System.out.println(sides.get(0) + sides.get(1) + sides.get(2));
+        }
+
+    }
 }
+
