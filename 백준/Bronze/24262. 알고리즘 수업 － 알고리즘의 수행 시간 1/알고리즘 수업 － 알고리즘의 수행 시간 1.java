4ccf5eae62014bc18c57b9c1dcdb771c7f3110b3
박시형 <71202869+hyeongsi@users.noreply.github.com>
2025-06-30 20:04:54 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 92 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -1,6 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-	public static void main(String[] args) {	
-		System.out.println(1);
-		System.out.println(0);
-	}
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(1);
+        System.out.println(0);
+    }
 }
+
