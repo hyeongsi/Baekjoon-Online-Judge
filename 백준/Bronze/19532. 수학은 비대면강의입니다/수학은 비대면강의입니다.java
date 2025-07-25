e619036c88d7f5f8d68025131a339287f8491fe4
박시형 <71202869+hyeongsi@users.noreply.github.com>
2025-07-26 08:54:31 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 168 ms, Memory: 16376 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -1,27 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
-	public static void main(String[] args) throws IOException {	
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
-		int c = Integer.parseInt(st.nextToken());
-		
-		int d = Integer.parseInt(st.nextToken());
-		int e = Integer.parseInt(st.nextToken());
-		int f = Integer.parseInt(st.nextToken());
-		
-		for(int i = -999; i <= 999; i++) {
-			for(int j = -999; j <= 999; j++) {
-				if((a * i + b * j == c) && (d * i + e * j == f)) {
-					System.out.println(i + " " + j);
-				}
-			}
-		}	
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        for (int x = -999; x <= 999; x++) {
+            for (int y = -999; y <= 999; y++) {
+                if ((input[0] * x) + (input[1] * y) == input[2] && (input[3] * x) + (input[4] * y) == input[5]) {
+                    System.out.println(x + " " + y);
+                    return;
+                }
+            }
+        }
+
+    }
 }
