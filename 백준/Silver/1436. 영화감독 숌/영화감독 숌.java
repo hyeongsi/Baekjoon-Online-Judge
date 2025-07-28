ed63abc09f7eddfee4ebc0496ab86b7aad930db1
박시형 <71202869+hyeongsi@users.noreply.github.com>
2025-07-28 18:53:49 +0900

    [Silver V] Title: 영화감독 숌, Time: 264 ms, Memory: 87928 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -1,24 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-	public static void main(String[] args) throws IOException {	
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int n = Integer.parseInt(br.readLine());
-		
-		int cnt = 0;
-		long i = 0;
-		while(true) {
-			i++;
-			if(Long.toString(i).contains("666"))
-				cnt++;
-			
-			if(cnt == n) {
-				System.out.println(i);
-				return;
-			}
-		}
-	}			
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int i = Integer.parseInt(br.readLine());
+        List<Long> titleList = new ArrayList<>();
+
+        long loopCnt = 0;
+        while (i != titleList.size()) {
+            if (-1 != Long.toString(loopCnt).indexOf("666")) {
+                titleList.add(loopCnt);
+            }
+            loopCnt++;
+        }
+
+        System.out.println(titleList.get(i - 1));
+    }
 }
