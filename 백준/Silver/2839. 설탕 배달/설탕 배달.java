26f0ac6ce4da30ceee459ae31b538187629a514f
박시형 <71202869+hyeongsi@users.noreply.github.com>
2025-07-30 19:59:15 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -3,25 +3,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException {	
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int n = Integer.parseInt(br.readLine());
-		int[] sugar = {5,3};
-		int ans = -1;
-		
-		int fCnt = n / sugar[0];
-		while(fCnt >= 0) {
-			int rest = n - (sugar[0] * fCnt);
-			
-			if(rest % sugar[1] == 0) {
-				ans = fCnt + (rest/sugar[1]);
-				break;
-			}
-				
-			fCnt--;
-		}
-		
-		System.out.println(ans);
-	}			
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int i = Integer.parseInt(br.readLine());
+
+        final int hSugar = 5;
+        final int lSugar = 3;
+
+        int ans = -1;
+
+        int cnt = i / hSugar;
+        while (cnt >= 0) {
+            int rest = i - (hSugar * cnt);
+
+            if (rest % lSugar == 0) {
+                ans = cnt + (rest / lSugar);
+                break;
+            }
+
+            cnt--;
+        }
+
+        System.out.println(ans);
+    }
 }
