10dde44ec890c2684e3fae1563692687060dc6a2
박시형 <71202869+hyeongsi@users.noreply.github.com>
2025-08-18 22:03:15 +0900

    [Bronze II] Title: 커트라인, Time: 120 ms, Memory: 14564 KB -BaekjoonHub

--- 백준/Bronze/25305. 커트라인/커트라인.java
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -3,24 +3,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int n = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(st.nextToken());
-		
-		Integer[] arr = new Integer[n];
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		Arrays.sort(arr, Collections.reverseOrder());
-		
-		System.out.println(arr[k-1]);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int k = Integer.parseInt(br.readLine().split(" ")[1]);
+        Integer[] scores = Arrays.stream(br.readLine().split(" ")).map(Integer::valueOf).toArray(Integer[]::new);
+
+        Arrays.sort(scores, Collections.reverseOrder());
+        System.out.println(scores[k - 1]);
+    }
 }
