7ac7a2016ec124127787768efe5814fed36ceb31
박시형 <71202869+hyeongsi@users.noreply.github.com>
2025-08-23 21:57:11 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1376 ms, Memory: 117512 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,30 +1,23 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.io.*;
+import java.util.Arrays;
 
 public class Main {
-	public static void main(String[] args) throws IOException {	
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int n = Integer.parseInt(br.readLine());
-		ArrayList<Integer> arr = new ArrayList<>();
-		for(int i = 0; i < n; i++) {
-			int input = Integer.parseInt(br.readLine());
-			arr.add(input);
-		}
-		
-		Collections.sort(arr);
-		
-		for(int i : arr) {
-			bw.write(i + "\n");
-		}
-		
-		bw.flush();
-		bw.close();
-	}		
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        for (int i = 0; i < n; i++) {
+            bw.write(arr[i] + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+
+    }
 }
