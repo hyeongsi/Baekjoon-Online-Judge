767c277d4fa19f6d73b891cc0f6bfb94de65c360
박시형 <71202869+hyeongsi@users.noreply.github.com>
2025-10-09 11:27:23 +0900

    [Silver V] Title: 소트인사이드, Time: 104 ms, Memory: 14384 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,27 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		ArrayList<Integer> arr = new ArrayList<>();
-		
-		while(n > 0) {
-			arr.add(n%10);
-			n/=10;
-		}
-		
-		Collections.sort(arr);
-		
-		StringBuilder sb = new StringBuilder();
-		for(int i = arr.size()-1; i >= 0; i--) {
-			sb.append(arr.get(i));
-		}
-		
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] split = br.readLine().split("");
+        List<String> list = Arrays.asList(split);
+
+        Collections.sort(list);
+        Collections.reverse(list);
+
+        list.forEach(System.out::print);
+    }
 }
