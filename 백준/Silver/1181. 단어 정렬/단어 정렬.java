4f1b5d4198b1e1c5a17278a1ce0c80e6eacdc25f
박시형 <71202869+hyeongsi@users.noreply.github.com>
2025-10-13 22:07:43 +0900

    [Silver V] Title: 단어 정렬, Time: 308 ms, Memory: 22756 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,46 +1,30 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.List;
+import java.io.*;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
-	public static void main(String[] args) throws IOException {	
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int k = Integer.parseInt(br.readLine());
-		Set<String> set = new HashSet<>();
-		for(int i = 0; i < k; i++) {
-			set.add(br.readLine());
-		}
-		
-		List<String> list = new ArrayList<>(set);
-		Collections.sort(list, new Comparator<String>() {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-			@Override
-			public int compare(String o1, String o2) {
-				int o1l = o1.length();
-				int o2l = o2.length();
-				
-				if(o1l > o2l) {
-					return 1;
-				}
-				else if(o1l < o2l) {
-					return -1;
-				}
-				else {
-					return o1.compareTo(o2);
-				}
-			}
-		});
-		
-		for(String s: list) {
-			System.out.println(s);
-		}
-		
-	}	
+        int n = Integer.parseInt(br.readLine());
+
+        Set<String> set = new TreeSet<>((o1, o2) -> {
+            if (o1.length() == o2.length()) {
+                return o1.compareTo(o2);
+            }
+            return o1.length() - o2.length();
+        });
+
+        for (int i = 0; i < n; i++) {
+            set.add(br.readLine());
+        }
+
+        for (String str : set) {
+            bw.write(str + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
 }
