cd953b19e9f485880eda5008e9507aac592667c6
박시형 <71202869+hyeongsi@users.noreply.github.com>
2025-10-15 19:00:57 +0900

    [Silver II] Title: 좌표 압축, Time: 1852 ms, Memory: 266492 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,39 +1,36 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		
-		int[] arr1 = new int[n];
-		int[] arr2 = new int[n];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < n; i++) {
-			arr1[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		System.arraycopy(arr1, 0, arr2, 0, arr1.length);
-		
-		Arrays.sort(arr2);
-		HashMap<Integer, Integer> hashMap = new HashMap<>();
-		
-		int r = 0;
-		for(int v : arr2) {
-			if(!hashMap.containsKey(v)) {
-				hashMap.put(v, r++);
-			}
-		}
-		
-		StringBuilder sb = new StringBuilder();
-		for(int k : arr1) {
-			sb.append(hashMap.get(k) + " ");
-		}
-		
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] orgNum = new int [n];
+        int[] sortedNum = new int[n];
+        Map<Integer, Integer> rankMap = new HashMap<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            orgNum[i] = Integer.parseInt(st.nextToken());
+            sortedNum[i] = orgNum[i];
+        }
+
+        Arrays.sort(sortedNum);
+
+        int rank = 0;
+        for (int num : sortedNum) {
+            if (!rankMap.containsKey(num)) {
+                rankMap.put(num, rank++);
+            }
+        }
+
+        for (int num : orgNum) {
+            bw.write(rankMap.get(num) + " ");
+        }
+
+        bw.flush();
+        bw.close();
+    }
 }
