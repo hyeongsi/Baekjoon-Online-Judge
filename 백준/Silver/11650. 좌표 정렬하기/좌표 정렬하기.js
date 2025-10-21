6e6d960833ba43a036848f7ffd94290dc159a034
박시형 <71202869+hyeongsi@users.noreply.github.com>
2025-10-21 23:12:59 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 456 ms, Memory: 63528 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const [num, ...posStr] = fs
+  .readFileSync("./dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+const pos = [];
+posStr.forEach((value, index, array) => {
+  pos.push(value.split(" ").map(Number));
+});
+
+pos.sort((a, b) => {
+  if (a[0] === b[0]) {
+    return a[1] - b[1];
+  }
+  return a[0] - b[0];
+});
+
+let ans = "";
+pos.forEach((value, index, array) => {
+  ans += value.join(" ") + "\n";
+});
+console.log(ans);
