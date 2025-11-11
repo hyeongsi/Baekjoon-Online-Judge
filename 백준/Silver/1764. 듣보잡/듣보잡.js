308bfebe55f76af2c72e5fd6022b6ef3c47eed36
박시형 <71202869+hyeongsi@users.noreply.github.com>
2025-11-11 22:26:28 +0900

    [Silver IV] Title: 듣보잡, Time: 188 ms, Memory: 25340 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.js
+++ 백준/Silver/1764. 듣보잡/듣보잡.js
@@ -1,11 +1,12 @@
-const input = require('fs').readFileSync(0).toString().trim().split(/\r?\n/);
+const fs = require("fs");
+const input = fs.readFileSync("./dev/stdin").toString().trim().split("\n");
 
-const [n, m] =input[0].split(" ").map(Number);
-const nPeople = input.slice(1, n + 1);
-const mPeople = input.slice(n + 1);      
-const target = new Set(nPeople);
+const [n, m] = input[0].split(" ").map(Number);
+const dm = input.slice(1, n + 1);
+const bm = input.slice(n + 1);
 
-const result = mPeople.filter((i) => target.has(i)).sort();
+const dmSet = new Set(dm);
+const result = bm.filter((i) => dmSet.has(i)).sort();
 
-console.log(result.length + "")
-console.log(result.join("\n"))
+console.log(result.length);
+console.log(result.join("\n"));
